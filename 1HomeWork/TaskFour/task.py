2f34dbd5349525fe597c8f7af59bfe2d6ea44eeb
Igor <adel-time@mail.ru>
2022-12-18 10:07:41 +0800

    Решил вторую задачу первого семинара по пайтон

---
+++ 1HomeWork/TaskFour/task.py
@@ -0,0 +1,18 @@
+# // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+# Console.WriteLine("Введите X1");
+# int x1 = int.Parse(Console.ReadLine()!);
+# Console.WriteLine("Введите Y1");
+# int y1 = int.Parse(Console.ReadLine()!);
+# Console.WriteLine("Введите Z1");
+# int z1 = int.Parse(Console.ReadLine()!);
+# Console.WriteLine("Введите X2");
+# int x2 = int.Parse(Console.ReadLine()!);
+# Console.WriteLine("Введите Y2");
+# int y2 = int.Parse(Console.ReadLine()!);
+# Console.WriteLine("Введите Z2");
+# int z2 = int.Parse(Console.ReadLine()!);
+
+# int Lenght = 0;
+
+
+# Console.WriteLine($"Lenght = {Math.Sqrt(Lenght = ((x2 - x1)*(x2 - x1)) + ((y2 - y1)*(y2 - y1)) + ((z2 - z1)*(z2 - z1)))}");
