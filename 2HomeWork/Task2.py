df88584e106a88e8013da14d539a08369ddde8f8
Igor <adel-time@mail.ru>
2022-12-24 13:00:37 +0800

    ыполнена третья задача второго домашнего задания

--- 2HomeWork/Task2.py
+++ 2HomeWork/Task2.py
@@ -3,11 +3,11 @@
 # Для n=4 -> [2, 2.25, 2.37, 2.44]
 # Сумма 9.06
 
-# number = int(input('Введите число: ')) 
+number = int(input('Введите число: ')) 
 
-# def funkciya(n):
-#     return [round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]
+def funkciya(n):
+    return [round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]
 
-# nums = funkciya(number)
-# print(nums)
-# print(round(sum(nums), 2))
+nums = funkciya(number)
+print(nums)
+print(round(sum(nums), 2))
