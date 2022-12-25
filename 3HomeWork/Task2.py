2e2aaa36293904c0da76e60ad8f30345e5fed880
Igor <adel-time@mail.ru>
2022-12-25 12:19:01 +0800

    Решил третье задание Третьей Д/З

--- 3HomeWork/Task2.py
+++ 3HomeWork/Task2.py
@@ -3,15 +3,15 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
-my_list = [2, 3, 4, 5, 6]
-a = 0
-if (len(my_list)%2 !=0):
-    l = len(my_list)//2 + 1
-if (len(my_list)%2 ==0):
-    l = len(my_list)//2
-countRevers = len(my_list) - 1
-for i in range(l):
-    a = 0
-    a = my_list[i] * my_list[countRevers]
-    countRevers = countRevers - 1 
-    print(a)
+# my_list = [2, 3, 4, 5, 6]
+# a = 0
+# if (len(my_list)%2 !=0):
+#     l = len(my_list)//2 + 1
+# if (len(my_list)%2 ==0):
+#     l = len(my_list)//2
+# countRevers = len(my_list) - 1
+# for i in range(l):
+#     a = 0
+#     a = my_list[i] * my_list[countRevers]
+#     countRevers = countRevers - 1 
+#     print(a)
