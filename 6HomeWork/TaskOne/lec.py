0f091bea7874b087ff7b86cc11fd3f1af484e0b9
Igor <adel-time@mail.ru>
2023-01-22 12:27:00 +0800

    домашнее задание6

---
+++ 6HomeWork/TaskOne/lec.py
@@ -0,0 +1,43 @@
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+
+
+# number = float(input('Введите вещественное число '))
+# result = 0
+# if number != int(number):
+#     number = round(number, 7)
+#     print(number)
+#     while (number != int(number)):
+#         number = number * 10
+#         print(int(number))
+# # if number == int(number):
+# while (1 <= number):
+#     result = result + number % 10
+#     number = number / 10
+# print(int(result))
+
+#Реализация с помощью новых операторов
+# Формат: Объясняет преподаватель
+
+# Задача: предложить улучшения кода для уже решённых задач:
+
+# С помощью использования **лямбд, filter, map, zip, enumerate, list comprehension
+
+number = int(input("Введите вещественное число "))
+list = []
+def result(x):
+    
+
+
+# list = []
+# for i in range(1, 101):#Заполняем список четными числами
+#     if (i%2 ==0):
+#         list.append(i);#Можно ли это сделать более красиво?
+# print(list)#Печатаем список, проверяем, что все числа четные
+# #Можем обрабатывать данные
+# def f(x):
+#     return x**3#Функция возводит число в третью степень
+
+# list = [f(i) for i in range(1,21) if i % 2 == 0]
