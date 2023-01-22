26cad0d684b5a63bddcb0cda0e998b0b35c5f3d6
Igor <adel-time@mail.ru>
2023-01-22 10:36:17 +0800

    домашнее задание5.1

--- 5HomeWork/TaskTwo/lec.py
+++ 5HomeWork/TaskTwo/lec.py
@@ -12,81 +12,316 @@ my_listGame = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 my_list = ['1', '2', '3']
 my_list2 = ['4', '5', '6']
 my_list3 = ['7', '8', '9']
+my_listComp = []
+my_listUser = []
 playComp = 0
 count = 8
 temp = 0
+countWinComp = 0
+countWinUser = 0
 #my_listResult = []
-for i in range(6):
-    numberRandom = random.randint(0,count)
-    temp = numberRandom
-    print(numberRandom)
-    print(my_listGame)
-    numberRandom = my_listGame[numberRandom]
-    print(f"Компьютер закрывает ячейку {numberRandom}")
-    if numberRandom >= 0 and numberRandom < 4:
-        if numberRandom != 0:
-            numberRandom = numberRandom -1
-        my_list[numberRandom] = '0'
-    elif numberRandom > 3 and numberRandom < 7:
-        numberRandom = numberRandom - 4
-        my_list2[numberRandom] = '0'
-    elif numberRandom > 6 and numberRandom < 10:
-        numberRandom = numberRandom - 7
-        my_list3[numberRandom] = '0'
-    my_listGame.pop(temp)
-    count = count - 1
-    print(my_list)
-    print(my_list2)
-    print(my_list3)
-    number = int(input("Какую ячейку закроешь ты "))
-    #number2 = number
-    for i in range(9):
-        # print(i)
-        # print(my_listGame[i])
-        # print(number)
-        if my_listGame[i] == number:
-            temp = i
-            print(i)
-        break
-        
-    if number >= 0 and number < 4:
-        if numberRandom != 0:
-            numberRandom = numberRandom -1
-        my_list[i] = 'X'
-        for i in range(2):
-            if my_list[i] == number:
-                my_list[i] = 'X'
+if (numberGamer == 2):
+    for i in range(10):
+        numberRandom = random.randint(0,count)
+        #print(f'Индекс numberRandom {numberRandom}')
+        temp = numberRandom
+        playComp = my_listGame[numberRandom]
+            
+        #print(my_listGame)
+        print(f"Компьютер закрывает ячейку {playComp}")
+        if (playComp >= 0 and playComp < 4):
+            #if (numberRandom != 0):
+            playComp = playComp -1
+            my_list[playComp] = '0'
+        elif (playComp > 3 and playComp < 7):
+            playComp = playComp - 4
+            my_list2[playComp] = '0'
+        elif (playComp > 6 and playComp < 10):
+            playComp = playComp - 7
+            my_list3[playComp] = '0'
+        my_listComp.append(my_listGame[temp])
         my_listGame.pop(temp)
-    # elif number2 > 3 and number2 < 7:
-    #     for i in my_list2:
-    #         if i == number2:
-    #             my_list2[i] = 'X'
-    #         #continue
-    #     #number = number - 4
-        
-    #     my_listGame.pop(temp)
-    # elif numberRandom > 6 and numberRandom < 10:
-    #     number = number - 7
-    #     my_list3[number] = 'X'
-    #     my_listGame.pop(temp)
-    # count = count - 1
-    print(my_list)
-    print(my_list2)
-    print(my_list3)
+        count = count - 1
+        print(my_list)
+        print(my_list2)
+        print(my_list3)
+        if (len(my_listGame) == 0):
+            print("Игра закончена")
+            break
 
+        #print(my_listGame)
+        number = int(input("Какую ячейку закроешь ты "))
+        for i in range(len(my_listGame)):
+            if (number == my_listGame[i]):
+                temp = i
+                #print(f'Индекс удаляемого элемента + {temp}')
+        if (number >= 0 and number < 4):
+            number = number -1
+            my_list[number] = 'X'
+        elif (number > 3 and number < 7):
+            my_list2[number - 4] = 'X'
+        elif (number > 6 and number < 10):
+            number = number - 7
+            my_list3[number] = 'X'
+        my_listUser.append(my_listGame[temp])
+        my_listGame.pop(temp)
+        count = count - 1
+        print(my_list)
+        print(my_list2)
+        print(my_list3)
+        print(f"Комбинация компьбтера {my_listComp}")
+        print(f"Комбинация игрока {my_listUser}")
+        print()
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==1 or my_listComp[i] == 2 or my_listComp[i] == 3):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==1 or my_listComp[i] == 4 or my_listComp[i] == 7):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==2 or my_listComp[i] == 5 or my_listComp[i] == 8):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==3 or my_listComp[i] == 6 or my_listComp[i] == 9):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==4 or my_listComp[i] == 5 or my_listComp[i] == 6):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==7 or my_listComp[i] == 8 or my_listComp[i] == 9):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==1 or my_listComp[i] == 5 or my_listComp[i] == 9):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==7 or my_listComp[i] == 5 or my_listComp[i] == 3):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+            
+            
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==1 or my_listUser[i] == 2 or my_listUser[i] == 3):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==1 or my_listUser[i] == 4 or my_listUser[i] == 7):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==2 or my_listUser[i] == 5 or my_listUser[i] == 8):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==3 or my_listUser[i] == 6 or my_listUser[i] == 9):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==4 or my_listUser[i] == 5 or my_listUser[i] == 6):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==7 or my_listUser[i] == 8 or my_listUser[i] == 9):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==1 or my_listUser[i] == 5 or my_listUser[i] == 9):
+                countWinUser = countWinUser +1
+            if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==7 or my_listUser[i] == 5 or my_listUser[i] == 3):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        if (len(my_listGame) == 0):
+            print("Игра закончена")
+            break
 
+if (numberGamer == 1):
+    for i in range(10):
+        number = int(input("Какую ячейку закроешь ты "))
+        for i in range(len(my_listGame)):
+            if (number == my_listGame[i]):
+                temp = i
+                #print(f'Индекс удаляемого элемента + {temp}')
+        if (number >= 0 and number < 4):
+            number = number -1
+            my_list[number] = 'X'
+        elif (number > 3 and number < 7):
+            my_list2[number - 4] = 'X'
+        elif (number > 6 and number < 10):
+            number = number - 7
+            my_list3[number] = 'X'
+        my_listUser.append(my_listGame[temp])
+        my_listGame.pop(temp)
+        count = count - 1
+        print(my_list)
+        print(my_list2)
+        print(my_list3)
+        if (len(my_listGame) == 0):
+            print("Игра закончена")
+            break
 
-# import random
-
-# my_list =[]
-# for i in range(10):
-#     my_list.append(random.randint(0,100))
-# print(my_list)
-# count = 10
-# my_listResult = []
-# for i in range(10):
-#     numberRandom = random.randint(0,count)
-#     my_listResult.append(my_list [numberRandom])
-#     count = count - 1
-#     my_list.pop(numberRandom)
-# print(my_listResult)
+        numberRandom = random.randint(0,count)
+        #print(f'Индекс numberRandom {numberRandom}')
+        temp = numberRandom
+        playComp = my_listGame[numberRandom]
+            
+        #print(my_listGame)
+        print(f"Компьютер закрывает ячейку {playComp}")
+        if (playComp >= 0 and playComp < 4):
+            #if (numberRandom != 0):
+            playComp = playComp -1
+            my_list[playComp] = '0'
+        elif (playComp > 3 and playComp < 7):
+            playComp = playComp - 4
+            my_list2[playComp] = '0'
+        elif (playComp > 6 and playComp < 10):
+            playComp = playComp - 7
+            my_list3[playComp] = '0'
+        my_listComp.append(my_listGame[temp])
+        my_listGame.pop(temp)
+        count = count - 1
+        print(my_list)
+        print(my_list2)
+        print(my_list3)
+        if (len(my_listGame) == 0):
+            print("Игра закончена")
+            break
+        print(f"Комбинация компьбтера {my_listComp}")
+        print(f"Комбинация игрока {my_listUser}")
+        print()
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==1 or my_listComp[i] == 2 or my_listComp[i] == 3):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==1 or my_listComp[i] == 4 or my_listComp[i] == 7):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==2 or my_listComp[i] == 5 or my_listComp[i] == 8):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==3 or my_listComp[i] == 6 or my_listComp[i] == 9):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==4 or my_listComp[i] == 5 or my_listComp[i] == 6):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==7 or my_listComp[i] == 8 or my_listComp[i] == 9):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==1 or my_listComp[i] == 5 or my_listComp[i] == 9):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+        countWinComp = 0
+        for i in range(len(my_listComp)):
+            if (my_listComp[i] ==7 or my_listComp[i] == 5 or my_listComp[i] == 3):
+                countWinComp = countWinComp +1
+                if (countWinComp == 3):
+                    print("Компьютер выиграл")
+            
+            
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==1 or my_listUser[i] == 2 or my_listUser[i] == 3):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==1 or my_listUser[i] == 4 or my_listUser[i] == 7):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==2 or my_listUser[i] == 5 or my_listUser[i] == 8):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==3 or my_listUser[i] == 6 or my_listUser[i] == 9):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==4 or my_listUser[i] == 5 or my_listUser[i] == 6):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==7 or my_listUser[i] == 8 or my_listUser[i] == 9):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==1 or my_listUser[i] == 5 or my_listUser[i] == 9):
+                countWinUser = countWinUser +1
+            if (countWinUser == 3):
+                    print("Игорок выиграл")
+        countWinUser = 0
+        for i in range(len(my_listUser)):
+            if (my_listUser[i] ==7 or my_listUser[i] == 5 or my_listUser[i] == 3):
+                countWinUser = countWinUser +1
+                if (countWinUser == 3):
+                    print("Игорок выиграл")
+        if (len(my_listGame) == 0):
+            print("Игра закончена")
+            break
